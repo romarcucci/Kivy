fcdced5b44146e4ac954d88f38c40472fbda7793
Romain Marcucci <romain.marcucci@gmail.com>
2018-01-12 13:01:30 +0100

    fake exp

--- firstTry/main.py
+++ firstTry/main.py
@@ -3,44 +3,56 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
-   
-startPosX = 150
-startPosY = 150
-
-goalX = 750
-goalY = 550
+from kivy.clock import Clock
 
 class MyPaintWidget(Widget):
+    posX = 150
+    posY = 150   
+
+    goalX = 750
+    goalY = 550
 
     def setAgent(self):
         with self.canvas:
             Color(1, 0, 1, mode='hsv')
             d = 30.
-            Ellipse(pos=(startPosX - d / 2, startPosY - d / 2), size=(d, d))
-            print('Pos X: %s Pos Y: %s' %(startPosX, startPosY))
+            Ellipse(pos=(self.posX - d / 2, self.posY - d / 2), size=(d, d))
+            print('Pos X: %s Pos Y: %s' %(self.posX, self.posY))
 
     def setGoal(self):
         with self.canvas:
             Color(0, 1, 1, mode='hsv')
             d = 30.
-            Ellipse(pos=(goalX - d / 2, goalY - d / 2), size=(d, d))
-            print('Goal X: %s Goal Y: %s' %(goalX, goalY))
+            Ellipse(pos=(self.goalX - d / 2, self.goalY - d / 2), size=(d, d))
+            print('Goal X: %s Goal Y: %s' %(self.goalX, self.goalY))
+    
+    def update(self, dt):
+        with self.canvas:
+
+            Color(0, 0, 0, mode='hsv')
+            d = 30.
+            Ellipse(pos=(self.posX - d / 2, self.posY - d / 2), size=(d, d))
+
+            if self.posX < self.goalX: self.posX = self.posX+1
+            if self.posX > self.goalX: self.posX = self.posX-1
+            if self.posY < self.goalY: self.posY = self.posY+1
+            if self.posY > self.goalY: self.posY = self.posY-1
+            
+            Color(1, 0, 1, mode='hsv')
+            d = 30.
+            Ellipse(pos=(self.posX - d / 2, self.posY - d / 2), size=(d, d))
+            print('Pos X: %s Pos Y: %s' %(self.posX, self.posY))
 
 class MyPaintApp(App):
 
     def build(self):
         parent = MyPaintWidget()
         self.painter = MyPaintWidget()
-        startBtn = Button(text='Start')
-        startBtn.bind(on_release=self.start_exp)
         parent.add_widget(self.painter)
-        parent.add_widget(startBtn)
         parent.setAgent()
         parent.setGoal()
+        Clock.schedule_interval(parent.update, 0.01)
         return parent
-
-    def start_exp(self, obj):
-        self.painter.canvas.clear()
 
 if __name__ == '__main__':
     MyPaintApp().run()
